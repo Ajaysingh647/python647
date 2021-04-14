d24e6c3b22fc5b540bcdf898932fb69d77b452da
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-04-14 13:00:25 +0530

    to_copy_elements_of_any_string_to_any_null_string

---
+++ copy_a_str.py
@@ -0,0 +1,15 @@
+# for copy any string (user defined)
+
+
+r='python using pycharm'
+t=''
+s=r+t
+print(s)
+
+
+#for copy any string given by the user
+
+r=input('Enter the string')
+t=''
+s=r+t
+print(s)
