97f25fe95e1eb17687aa8baf25c3bf57829bd9cf
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-04-14 12:42:02 +0530

    Find_the_len_of_any_str

---
+++ Lenght_of_str.py
@@ -0,0 +1,15 @@
+#when the data is given
+
+re='ajay singh python and pycharm'
+st=len(re)
+print(st)
+
+
+
+#when data is given by the user
+
+
+
+re=input('Enter the string')
+st=len(re)
+print(st)
