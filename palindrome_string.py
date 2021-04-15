8d7c75e9fcc1a5d6d06995b16dcbc305f07a85b4
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-04-15 11:50:39 +0530

    given string is palindrome or not

---
+++ palindrome_string.py
@@ -0,0 +1,20 @@
+# given String is Palindrome (user defined)
+r='ama'
+l=r[::-1]
+if r==l:
+    print('Given string is palindrome ')
+else:
+    print('given string is not palindrome')
+
+
+
+
+#To check whether that the string is palindrome or not (Given by the user)
+
+
+r=input('Enter the string:-')
+l=r[::-1]
+if r==l:
+    print('Given string is palindrome ')
+else:
+    print('given string is not palindrome')
