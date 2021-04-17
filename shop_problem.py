fad506acb97304f9cc7b0085cdb4ddeb840af321
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-04-17 21:28:14 +0530

    shop_problem

---
+++ shop_problem.py
@@ -0,0 +1,7 @@
+# shop problem
+re=int(input('Enter the amount:- '))
+if re>=1000:
+    pr=re*0.1
+    print(f'The amount to be given to shopkeeper:- {re+pr}')
+else:
+    print('No discount is given to te customer')
