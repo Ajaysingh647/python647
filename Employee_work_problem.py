f56f89e07d3035e31c7df79cbe3d6f5b71e05f1a
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-04-19 21:31:55 +0530

    Employee_work_problem

---
+++ Employee_work_problem.py
@@ -0,0 +1,5 @@
+# for giving the working area to the employee of any company
+age=int(input("Enter the age of the employee:- "))
+sex=int(input('Enter the sex of the employee:- '))
+ms=int(input('Enter the maritial status of the employee:- '))
+if
